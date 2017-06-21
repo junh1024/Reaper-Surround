44e91b6f77de798839f10f871b01f4efc133b093
junh1024 <junh1024@gmail.com>
2017-06-20 20:11:35 -0400

    Fix bug (not looping all tracks)

--- SetPanAccordingToName.py
+++ SetPanAccordingToName.py
@@ -2,7 +2,7 @@
 
 from reaper_python import *
 from contextlib import contextmanager
-import os
+# import os
 
 @contextmanager
 def undoable(message):
@@ -12,28 +12,16 @@ def undoable(message):
 	finally:
 		RPR_Undo_EndBlock2(0, message, -1)
 
+def msg(m):
+	RPR_ShowConsoleMsg(m)
+
 with undoable("Set Pan According To Track SUffiX"):
 
-	def msg(m):
-		RPR_ShowConsoleMsg(m)
-
-	# def renameToSourceFileName():
-	trackCount = RPR_CountSelectedTracks(0)
-	
-	# msg("hello wold")
-	
-	
-	
-		# if trackCount < 1:
-			# msg("")
-			# msg("Select tracks first")
-			# return
-
-	for i in range(trackCount):
-		trackId = RPR_GetSelectedTrack(0, i)
+	for i in range(RPR_CountTracks(0)):
+		trackId = RPR_GetTrack(0, i)
 		
 		thename = str(RPR_GetSetMediaTrackInfo_String(trackId, "P_NAME", "", False )[3] )
-		msg (thename)
+		# msg (thename)
 		
 		RPR_SetMediaTrackInfo_Value(trackId, "D_PAN", 0.15)
 		RPR_SetMediaTrackInfo_Value(trackId, "C_MAINSEND_OFFS", 1)
