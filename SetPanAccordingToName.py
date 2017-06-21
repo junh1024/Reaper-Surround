1713123fa64d029e9d7eb63231c3e87a8cb8d83b
junh1024 <junh1024@gmail.com>
2017-06-20 23:06:58 -0400

    Add files via upload

--- SetPanAccordingToName.py
+++ SetPanAccordingToName.py
@@ -20,9 +20,22 @@ with undoable("Set Pan According To Track SUffiX"):
 	for i in range(RPR_CountTracks(0)):
 		trackId = RPR_GetTrack(0, i)
 		
-		thename = str(RPR_GetSetMediaTrackInfo_String(trackId, "P_NAME", "", False )[3] )
-		# msg (thename)
+		suffix = str(RPR_GetSetMediaTrackInfo_String(trackId, "P_NAME", "", False )[3] )[-2:].lstrip().upper() #get actual track name, last 2 chars)
 		
-		RPR_SetMediaTrackInfo_Value(trackId, "D_PAN", 0.15)
-		RPR_SetMediaTrackInfo_Value(trackId, "C_MAINSEND_OFFS", 1)
-		 
+		if(suffix in ('C','RR')): #last 2 of naRR
+			# msg("suffix was C")
+			RPR_SetMediaTrackInfo_Value(trackId, "D_PAN", 0) #center pans
+			
+		if(suffix in ('C,RR,L,R,LW,RW,LC,RC'.split(','))): #anything front
+			RPR_SetMediaTrackInfo_Value(trackId, "C_MAINSEND_OFFS", 0)
+		# RPR_SetMediaTrackInfo_Value(trackId, "D_PAN", 0.15)
+		
+		if(suffix[0] == 'S'): #anything rear/surround
+			RPR_SetMediaTrackInfo_Value(trackId, "C_MAINSEND_OFFS", 4) #set rear/surround
+
+		# if(suffix == 'SL'): #anything rear/surround
+			# RPR_SetMediaTrackInfo_Value(trackId, "C_MAINSEND_OFFS", 4) #set rear/surround
+
+		# if(suffix == 'SR'): #anything rear/surround
+			# RPR_SetMediaTrackInfo_Value(trackId, "C_MAINSEND_OFFS", 4) #set rear/surround
+
