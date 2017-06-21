ffbc037544ee1601f16f714be87768494662d073
junh1024 <junh1024@gmail.com>
2017-06-20 23:37:18 -0400

    Refactor using more reflection

--- SetPanAccordingToName.py
+++ SetPanAccordingToName.py
@@ -27,6 +27,12 @@ RW=-LW
 LC=(2*L)/3
 RC=-LC
 
+#these have parenting set
+SL=-1
+SR=-SL
+
+RR=C=0 #last 2 of naRR,center pans
+
 # msg(L)
 # msg(R)
 # msg(LW)
@@ -40,24 +46,14 @@ with undoable("Set Pan According To Track SUffiX"):
 		trackId = RPR_GetTrack(0, i)
 		
 		suffix = str(RPR_GetSetMediaTrackInfo_String(trackId, "P_NAME", "", False )[3] )[-2:].lstrip().upper() #get actual track name, last 2 chars)
-		
-		if(suffix in ('C','RR')): #last 2 of naRR
-			# msg("suffix was C")
-			RPR_SetMediaTrackInfo_Value(trackId, "D_PAN", 0) #center pans
-			
+					
 		if(suffix in ('C,RR,L,R,LW,RW,LC,RC'.split(','))): #anything front
 			RPR_SetMediaTrackInfo_Value(trackId, "C_MAINSEND_OFFS", 0)
 		# RPR_SetMediaTrackInfo_Value(trackId, "D_PAN", 0.15)
 		
-		if(suffix in globals()): #if a suffix is one of the global preset pans
-			RPR_SetMediaTrackInfo_Value(trackId, "D_PAN", eval(suffix)) #set it according to the pan designated by the suffix. REFLECTION USED!
-		
 		if(suffix[0] == 'S'): #anything rear/surround
 			RPR_SetMediaTrackInfo_Value(trackId, "C_MAINSEND_OFFS", 4) #set rear/surround
 
-		if(suffix == 'SL'): #set SL pan
-			RPR_SetMediaTrackInfo_Value(trackId, "D_PAN", -1)
-
-		if(suffix == 'SR'): #set SR pan
-			RPR_SetMediaTrackInfo_Value(trackId, "D_PAN", 1)
-
+		if(suffix in globals()): #if a suffix is one of the global preset pans, see global variables above
+			RPR_SetMediaTrackInfo_Value(trackId, "D_PAN", eval(suffix)) #set it according to the pan designated by the suffix. REFLECTION USED!
+		
