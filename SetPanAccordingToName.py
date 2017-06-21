31eaae3168c35f5138ef402d34c090b9623a0c25
junh1024 <junh1024@gmail.com>
2017-06-21 01:24:33 -0400

    null handling

--- SetPanAccordingToName.py
+++ SetPanAccordingToName.py
@@ -13,7 +13,7 @@ def undoable(message):
 	finally:
 		RPR_Undo_EndBlock2(0, message, -1)
 
-# debug = True #disable for using
+debug = True #disable for using
 
 def msg(m):
 	if 'debug' in globals():
@@ -41,17 +41,22 @@ RR=C=0 #last 2 of naRR,center pans
 # msg(RC)
 
 with undoable("Set Pan According To Track Suffix"):
-
 	for i in range(RPR_CountTracks(0)): #for all tracks, get track
 		trackId = RPR_GetTrack(0, i)
 		
 		suffix = str(RPR_GetSetMediaTrackInfo_String(trackId, "P_NAME", "", False )[3] )[-2:].lstrip().upper() #get actual track name, last 2 chars, remove whitespace)
-					
-		if(suffix in ('C,RR,L,R,LW,RW,LC,RC'.split(','))): #anything front
-			RPR_SetMediaTrackInfo_Value(trackId, "C_MAINSEND_OFFS", 0)
+		
+		if(suffix==''):
+			continue
 		
 		if(suffix[0] == 'S'): #anything rear/surround. I'm not doing else cuz there may be top pans.
 			RPR_SetMediaTrackInfo_Value(trackId, "C_MAINSEND_OFFS", 4) #set parent ch 5/6 rear/surround
-
+		
 		if(suffix in globals()): #if a suffix is one of the global preset pans, see global variables above
-			RPR_SetMediaTrackInfo_Value(trackId, "D_PAN", eval(suffix)) #set it according to the pan designated by the suffix. REFLECTION USED!
+			RPR_SetMediaTrackInfo_Value(trackId, "D_PAN", eval(suffix)) #set it according to the pan designated by the suffix REFLECTION USED
+		
+		
+		if(suffix in ('C,RR,L,R,LW,RW,LC,RC'.split(','))): #anything front
+			RPR_SetMediaTrackInfo_Value(trackId, "C_MAINSEND_OFFS", 0)
+
+
