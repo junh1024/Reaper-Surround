50fa5a6da55c067c0798c05b7b89fbed1aafeee3
junh1024 <junh1024@gmail.com>
2017-11-21 17:58:07 -0500

    Add files via upload

--- ambi_encoder.py
+++ ambi_encoder.py
@@ -1,9 +1,6 @@
 from math import sin	,cos	,sqrt, radians
-# import os
 
-#TODO: remove if coeff <.1, account for varying w
-
-fuma_pan_equations="""sqrt(1/2)
+fuma_pan_equations="""sqrt(1/2)*slider4
 cos(A)*cos(E)
 sin(A)*cos(E)
 sin(E)
@@ -23,21 +20,11 @@ sin(3*A)*cos(E)*cos(E)*cos(E)"""
 fuma_order=['W','X','Y','Z','R','S','T','U','V','K','L','M','N','O','P','Q']
 
 r=2 #radius meters?,ignore
-z=0
-hz=45#height
-
-global A,E
-A=0#angle
-E=0#elevation
-
-w="slider2" #hex
-# w=45 #cine
+z=0#ELL elevation
+hz=45#height degrees
 
 f = open("jsfx_shell.txt", "w") 
 
-# slider1:1<0,2,0.1>Gain
-# slider2:30<0,90,2.5>Position of R (controls the width of all F & B pairs)
-# slider3:45<0,90,5>Height Elevation
 #pi is anticlockwise
 speaker_array = [
 'L',['(-A_rad)',r,z],
@@ -60,46 +47,58 @@ speaker_array = [
 'HSL',['(-(-$pi/2))',r,'E_rad'],
 'HSR',['((-$pi/2))',r,'E_rad']
 ]
-f.write('@init\n')
+f.write("""desc: 15.1 to 3oA Fuma downmixer. LFE is moved to C, so gain -10dB or silence it.
+//Generated from ambi_encoder.py
+
+slider1:30<0,90,2.5>Angle of R (influences speaker width)
+slider2:45<0,90,5>Elevation of height speakers
+slider3:1<0,2,0.1>Gain
+slider4:1<0,2,0.1>W Gain
+slider5:1<0,2,0.1>Reserved
+
+@init
+A_rad=slider1*($pi/-180);
+E_rad=slider2*($pi/180);
+""")
 
 coefficientnames=[]
 
+#make initial speaker positions
 for speaker in range (0, int(len(speaker_array)/2 )):
 	f.write(speaker_array[speaker*2]+'_A='+str(speaker_array[speaker*2+1][0])+';\n')
 	f.write(speaker_array[speaker*2]+'_E='+str(speaker_array[speaker*2+1][2])+';\n')
 	
-f.write('@slider') 
 f.write("""
-A_rad=slider2*($pi/-180);
-E_rad=slider3*($pi/180);
-"""
-) 
+@slider
+A_rad=slider1*($pi/-180);
+E_rad=slider2*($pi/180);
+""") 
 
-
-count=0                                                 
+count=0
+#make pan equations in @slider
 for line in fuma_pan_equations.splitlines():
 	for speaker in range (0, int(len(speaker_array)/2 )):
 		variablename=fuma_order[count]+"_coeff_"+speaker_array[speaker*2]
 		coefficientnames.append(variablename)
 		editedline=line.replace('A',str(speaker_array[speaker*2+1][0]))
 		editedline=editedline.replace('E',str(speaker_array[speaker*2+1][2]))
-		f.write(variablename+'='+editedline+';\n')
+		f.write(variablename+'='+editedline+'*slider3;\n')
 		# write=line
 	count+=1
 
-
 # print (speaker_array)
 # restrictions=[A,E,sin,cos,sqrt,radians]
-f.write('@sample\n')
-#create temp variables
+f.write('\n@sample\n')
+
+#create temp variables for speakers
 in_chans=16
 for in_chan in range (0,in_chans):
 	f.write(str('spl'+str(in_chan)+'_in=spl'+str(in_chan)+';\n'))
-
-
+f.write('\n')
 
 count=0
 coefficientnamescounter=0
+#write coefficients, multiply each speaker in with fuma coefficient for each fuma chan
 for line in fuma_pan_equations.splitlines():
 	
 	f.write(str('spl'+str(count)+'='))
@@ -117,7 +116,3 @@ for line in fuma_pan_equations.splitlines():
 	
 f.close
 # input("Press Enter to continue")
-
-
-
-# //calculate pan coeffs reduces CPU further. These now only happen per slider instead of per sample.
