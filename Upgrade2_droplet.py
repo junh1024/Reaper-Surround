2bf1e127e8a3bda3b11fd6e5296f93dbfc117814
junh1024 <junh1024@gmail.com>
2018-05-06 21:28:18 +1200

    now in-place replace

--- Upgrade2_droplet.py
+++ Upgrade2_droplet.py
@@ -3,25 +3,24 @@
 import re, sys,string
 
 in_file=sys.argv[1]
- 	
-read_file = open(in_file).readlines()
 
+
+with open(in_file,'r') as file:
+	lines = file.readlines()
 
 # insertionpoint = re.compile(r'^.*MASTER_NCH.*$')
 insertionpoint = re.compile(r'^.*MASTER_NCH.*$')
 
-write_file = open(in_file+"_S.RPP", "w") 
 
-
-for line in read_file:
+for index, line in enumerate(lines):
 	
 	m1 = insertionpoint.search(line)
 	# m2 = fileInProjPat.search(line)
 	if m1:
-		line= " MASTERHWOUT 2 0 1.00000000000000 0.00000000000000 0 0 2 -1.00000000000000\r\n" + line
-	
-	write_file.write(line)
-	# print("working")
-	# 
-write_file.close
-# input("Press Enter to continue...")
+		lines[index]= " MASTERHWOUT 2 0 1.00000000000000 0.00000000000000 0 0 2 -1.00000000000000\n" + line
+
+
+with open(in_file, "w")  as file:
+	file.write(''.join(lines))
+
+# input("waiting...")
