46a222150462c0ba1be7d5d7dc47157c41d2a015
junh1024 <junh1024@gmail.com>
2021-05-17 16:07:54 +1300

    new script, better 7.1 panning in 15.1 GUI panner

---
+++ Scripts/Reverse Take Pan.py
@@ -0,0 +1,40 @@
+desc=""
+by="junh1024"
+
+from reaper_python import *
+from contextlib import contextmanager
+
+@contextmanager
+def undoable(message):
+	RPR_Undo_BeginBlock2(0)
+	try:
+		yield
+	finally:
+		RPR_Undo_EndBlock2(0, message, -1)
+
+debug = True
+
+def msg(m):
+	if 'debug' in globals():
+		RPR_ShowConsoleMsg(m)
+
+# msg(RPR_CountSelectedMediaItems(0))
+		
+with undoable(desc):
+	# RPR_ShowConsoleMsg(BPM_proj)
+	
+	#get file BPM
+	# dialogstring="amount"
+	# theoffset = RPR_GetUserInputs("Adjust take by how many semitones?",1,dialogstring,"",64)[4]
+	# theoffset = float(theoffset.strip())
+	# RPR_ShowConsoleMsg(new_gain_bits)
+	
+	#get items
+	for i in range (RPR_CountSelectedMediaItems( 0)):
+		MI=RPR_GetSelectedMediaItem(0,i)
+	
+		# set gain
+		take=RPR_GetActiveTake(MI)
+		original=RPR_GetMediaItemTakeInfo_Value( take, "D_PAN" )
+		RPR_SetMediaItemTakeInfo_Value( take, "D_PAN", -original  )
+		 
